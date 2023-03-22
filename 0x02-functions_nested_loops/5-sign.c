f61f3a687794fc48fbb3ca021d3c40dd011e928f
Stablemental <meriemsaghrouni@gmail.com>
2023-03-22 14:20:36 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - function that prints nembers sign
  *
@@ -7,22 +6,21 @@
  *
  * Return: Either 0 or 1 or -1
  */
-
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n < '0')
 	{
-		_putchar('+');
-		return (1);
+		_putchar('-');
+		return (-1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else if n < 0
+	else if (n > 0)
 	{
-		_putchar('-');
-		return (-1);
-	}
+		_putchar('+');
+		return (1);
+	}	
 }
