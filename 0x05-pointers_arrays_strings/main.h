d8e6a2297f6a8b6a6204ff321d1dbce25f4f1909
Stablemental <meriemsaghrouni@gmail.com>
2023-03-27 09:33:54 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet_x10(void);
+void print_alphabet (void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void reset_to_98(int *n);
+
+#endif
