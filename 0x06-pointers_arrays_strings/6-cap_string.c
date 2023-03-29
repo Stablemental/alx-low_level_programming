1b981502948a24066892780c8234a458b752fc2c
Stablemental <meriemsaghrouni@gmail.com>
2023-03-29 15:16:17 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if ((str[i - 1] == ' ' ||
+		if (str[i - 1] == ' ' ||
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
 		str[i - 1] == ',' ||
@@ -26,7 +26,7 @@ char *cap_string(char *str)
 		str[i -	1] == '}' ||
 		i == 0)
 
-			s[i] -= 32;
+			str[i] -= 32;
 		i++;
 	}
 	return (str);
