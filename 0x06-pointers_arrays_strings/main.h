6a574a521d59e9af001ae838f3b2a5ab1a07ae44
Stablemental <meriemsaghrouni@gmail.com>
2023-03-29 09:29:59 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet_x10(void);
+void print_alphabet (void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int print_last_digit(int);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+char *_strcat(char *dest, char *src);
+
+#endif
